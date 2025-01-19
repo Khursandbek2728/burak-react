8e15db69d679dc7ccf3ef914496f5f2c0ad67884
Token <axmadjonovxursandbek7@gmail.com>
2025-01-20 07:41:52 +0900

    fixed all problems

--- src/app/screens/homePage/Statistics.tsx
+++ src/app/screens/homePage/Statistics.tsx
@@ -1,30 +1,36 @@
-import { Box, Container, Stack } from "@mui/material";
-import React from "react";
-import Divider from "../../components/divider";
+import React from 'react';
+import { Box, Container, Stack } from '@mui/material';
+import Divider from '../../components/divider';
 
 export default function Statistics() {
   return (
-    <div className={"static-frame"}>
+    <div className="static-frame">
       <Container>
         <Stack className="info">
           <Stack className="static-box">
             <Box className="static-num">12</Box>
-            <Box classname="static-text">Restaurants</Box>
+            <Box className="static-text">Restaurants</Box>
           </Stack>
+
           <Divider height="64" width="2" bg="#E3C08D" />
+
           <Stack className="static-box">
             <Box className="static-num">8</Box>
-            <Box classname="static-text">Experience</Box>
+            <Box className="static-text">Experience</Box>
           </Stack>
+
           <Divider height="64" width="2" bg="#E3C08D" />
+
           <Stack className="static-box">
             <Box className="static-num">50+</Box>
-            <Box classname="static-text">Menu</Box>
+            <Box className="static-text">Menu</Box>
           </Stack>
+
           <Divider height="64" width="2" bg="#E3C08D" />
+
           <Stack className="static-box">
             <Box className="static-num">200+</Box>
-            <Box classname="static-text">Clients</Box>
+            <Box className="static-text">Clients</Box>
           </Stack>
         </Stack>
       </Container>
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,21 +1,26 @@
-import React from "react";
+import { Container } from "@mui/material";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
-import ActiveUsers from "../helpPage/ActiveUsers";
+import ActiveUsers from "./AcitveUsers";
 import Events from "./Events";
-import "../../../css/home.css";
+import '../../../css/home.css'
 
-export default function HomePage() {
-  return (
+
+
+
+ export default function HomePage() {
+    return (
     <div className={"homepage"}>
-      <Statistics />
-      <PopularDishes />
-      <NewDishes />
-      <Advertisement />
-      <ActiveUsers />
-      <Events />
-    </div>
-  );
-}
+      <Statistics/>
+      <PopularDishes/>
+      <NewDishes/>
+      <Advertisement/>
+      <ActiveUsers/>
+      <Events/>
+    </div>)
+
+    
+  }
+  
